7e9785b7a8f9a6b87102510e7d9bec7c94e7e20c
jumhuur <jumhuur123@hotmail.com>
2022-12-10 17:28:23 +0300

    10/12/2022

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ku soo dhawaw react js
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://takriim.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          shabakada takriim
         </a>
       </header>
     </div>
