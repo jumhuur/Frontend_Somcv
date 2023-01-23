cda8ed4ae054ce95d3edbb1ef43fc01bf20e6c6c
jumhuur <jumhuur123@hotmail.com>
2023-01-23 13:59:12 +0300

    01/23/2023

--- src/Components/Designs/Designs.jsx
+++ src/Components/Designs/Designs.jsx
@@ -43,9 +43,11 @@ function Designs({click}) {
                     </div>  
                     ))}
 
-                    {!cv && [0,1,2,3,4,5,6,7,8].map((Num) => (
+                    
+                    {!cv  && [0,1,2,3,4,5,6,7,8].map((Num) => (
                         <Cvskl key={Num} />
                     ))}
+
 
                 </div>
             </div>
--- src/Components/Jobs/OneJob/OneJob.jsx
+++ src/Components/Jobs/OneJob/OneJob.jsx
@@ -1,5 +1,6 @@
+import { Link } from "react-router-dom"
+import Footer from "../../Footer/Footer"
 import Nav from "../../Nav/Nav"
-
 function OneJob() {
     const Pagetitle = "Web Develeper neded"
     return (
@@ -12,6 +13,55 @@ function OneJob() {
             </div>
         </div>
         </div>
+        <div className="Xajiye_guud onejob">
+        <div className="xajiye">
+            <div className="job_one_container">
+                <div className="qayb_midi">
+                <div className="jobs_in_job n_ar">
+                 <div className="logo_job">
+                     <img src="/Images/Web.png" alt="sawir_shaqo" />
+                 </div>
+                 <div className="info_and_title">
+                    <Link to={`/job/689870`}>
+                        <div className="ciwaan_shaqo">
+                         <h2>Driver Texi Neded</h2>
+                         <div className="info">
+                         <h3><i className="fa-solid fa-location-dot"></i> Hargeis</h3>
+                         <h3><i className="fa-regular fa-clock"></i> Full-Time</h3>
+                         <h3><i className="fa-solid fa-user-tie"></i> 65</h3>
+                     </div>
+                     </div>
+                     </Link>  
+                 </div>
+                 <div className="actions_btn">
+                     <button className="btn_job_applay">Applay</button>
+                     <h3><i className="fa-regular fa-clock"></i> 9 days ago</h3>
+                 </div>
+                </div>
+                <div className="job_desc">
+                    {<>
+                        <p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p><p>sjdkhsajdhsajdas</p>
+<ul>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+<li>sajdhgsahdsa</li>
+
+</ul>                
+                    </>}
+                </div>
+                </div>
+                <div className="qayb_bidix">
+                    <p>Qayb Bidix</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        <Footer />
+
         </>
     )
 }
