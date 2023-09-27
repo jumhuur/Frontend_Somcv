c7ff8ff29aab809943b72c380697440737838f84
abu-amar jumhuur <jumhuur123@hotmail.com>
2023-09-27 17:48:49 +0300

    done

--- Backend2/index.js
+++ Backend2/index.js
@@ -1,48 +1,49 @@
-require("dotenv").config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
-const  cors = require("cors");
+const cors = require("cors");
 
 // app
-const app = express()
-const cvRouter = require('./Routes/Cv.js')
-const UserRouter = require('./Routes/Users')
-const DwonloadRouter = require("./Routes/Download")
-const UsercvRouters = require("./Routes/UserCv")
-
-
+const app = express();
+const cvRouter = require("./Routes/Cv.js");
+const UserRouter = require("./Routes/Users");
+const DwonloadRouter = require("./Routes/Download");
+const UsercvRouters = require("./Routes/UserCv");
 
 // init Midallware
-app.use((req,res, next) =>  {
-    res.header("Access-Control-Allow-Origin", true)
-    res.header("Access-Control-Allow-Private-Network", true)
-    next()
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", true);
+  res.header("Access-Control-Allow-Private-Network", true);
+  next();
+});
 
-app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials: true
-}
-// app.use(cors({
-//     origin:"http://5.182.18.172:8800",
-//     credentials: true
-// }
-))
-// rounting 
-app.use('/Api', UserRouter)
-app.use('/Api', DwonloadRouter)
-app.use('/Api', cvRouter)
-app.use('/Api', UsercvRouters)
-mongoose.set("strictQuery", false)
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-// init app
-app.listen(process.env.PORT, () => {
-    console.log(`server Is OK ${process.env.PORT}`)
-})
-})
-.catch((err) => {
-    console.log(err)
-})
-
+app.use(express.json());
+app.use(
+  cors(
+    {
+      origin: "http://localhost:3001",
+      credentials: true,
+    }
+    // app.use(cors({
+    //     origin:"http://5.182.18.172:8800",
+    //     credentials: true
+    // }
+  )
+);
+// rounting
+app.use("/Api", UserRouter);
+app.use("/Api", DwonloadRouter);
+app.use("/Api", cvRouter);
+app.use("/Api", UsercvRouters);
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // init app
+    app.listen(process.env.PORT, () => {
+      console.log(`server Is OK ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import Slider from 'infinite-react-carousel';
-function Slide() {
+// import React from 'react';
+// import Slider from 'infinite-react-carousel';
+// function Slide() {
 
-    return (
-        <>
-        <div className="slide">
-        <div className="haye">
-                <div className="catcards">
-                <Slider slidesToShow={3} className="slide_cards">
-                    <div className="card">
-                        card1
-                    </div>
-                    <div className="card">
-                        card2
-                    </div>
-                    <div className="card">
-                        card3
-                    </div>
-                    <div className="card">
-                        card4
-                    </div>
-                    <div className="card">
-                        card5
-                    </div>
-                    <div className="card">
-                        card6
-                    </div>
-                </Slider>
-                </div>
-            
-        </div>
-        </div>
+//     return (
+//         <>
+//         <div className="slide">
+//         <div className="haye">
+//                 <div className="catcards">
+//                 <Slider slidesToShow={3} className="slide_cards">
+//                     <div className="card">
+//                         card1
+//                     </div>
+//                     <div className="card">
+//                         card2
+//                     </div>
+//                     <div className="card">
+//                         card3
+//                     </div>
+//                     <div className="card">
+//                         card4
+//                     </div>
+//                     <div className="card">
+//                         card5
+//                     </div>
+//                     <div className="card">
+//                         card6
+//                     </div>
+//                 </Slider>
+//                 </div>
 
-        
-        </>
-    )
-}
+//         </div>
+//         </div>
 
+//         </>
+//     )
+// }
 
-
-export default Slide
+// export default Slide
