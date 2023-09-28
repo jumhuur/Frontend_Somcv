128d19eb9696806ae68d9a4cc806f9bbcc3a8de6
abu-amar jumhuur <jumhuur123@hotmail.com>
2023-09-28 08:16:14 +0300

    server origin

--- Backend2/index.js
+++ Backend2/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://frontend-somcv.vercel.app/",
+    origin: "https://somcv.vercel.app",
     credentials: true,
   })
 );
